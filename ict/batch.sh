d3c7b7f2da67dea477118d8105a910ad5fc6198e
mayuemura <mayuemura3818@gmail.com>
2016-02-14 17:24:27 +0900

    modify PRF.py

--- ict/batch.sh
+++ ict/batch.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 #leave_one_out cross_validation
-#./feature.py < train > train.f
+./feature.py < train_lmtd > train_lmtd.f
 
-#~okazaki/local/bin/crfsuite learn -a ap -g385 -x -p max_iterations=20 train.f > log_file.txt
+~okazaki/local/bin/crfsuite learn -a ap -g5 -x -p max_iterations=20 train_lmtd.f > log_file_5.txt
 
-./feature.py < splt_train > splt_train.f
+python PRF.py log_file_5.txt
 
-~okazaki/local/bin/crfsuite learn -a ap -p max_iterations=20 -m all.model splt_train.f
+~okazaki/local/bin/crfsuite learn -a ap -g10 -x -p max_iterations=20 train_lmtd.f > log_file_10.txt
 
-./feature.py < splt_test > splt_test.f
+python PRF.py log_file_10.txt
 
-~okazaki/local/bin/crfsuite tag -r -m all.model < splt_test.f | conlleval.py >> result.txt
+#~okazaki/local/bin/crfsuite learn -a ap -g387 -x -p max_iterations=20 train_lmtd.f > log_file_387.txt
+
+python PRF.py log_file_387.txt
+
+#./feature.py < splt_train_lmtd > splt_train_lmtd.f
+
+#~okazaki/local/bin/crfsuite learn -a ap -p max_iterations=20 -m emt.model splt_train_lmtd.f
+
+#./feature.py < splt_test_lmtd > splt_test_lmtd.f
+
+#~okazaki/local/bin/crfsuite tag -r -m emt.model < splt_test_lmtd.f | conlleval.py >> result.txt
 
